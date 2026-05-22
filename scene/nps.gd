1023e1a984552b44a8d65aaf8d577d0497d99136
Artem <qntmcmptr@gmail.com>
2026-05-23 04:22:47 +0700

    hh

--- scene/nps.gd
+++ scene/nps.gd
@@ -6,79 +6,127 @@ var talked = 0
 func _on_body_entered(body: Node2D) -> void:
 
 	if body.name.to_lower() == "player":
-		print(coins)
-		if (Global.coins < 5) and ((Global.talked == 0) or(Global.talked == 1)):
+		print(body.coins)
+		if Global.talked == 0 or (Global.talked == 1 and Global.coins < 5):
 			var story = [
-				"Привет! Ого, как ты тут оказался?
-				Редко кто заходит а наш город
-				(ЛКМ чтобы продолжить)",
+				"Привет! Ого, как ты тут оказался?\nРедко кто заходит в наш город\n(ЛКМ чтобы продолжить)",
 				"Ты заблудился?",
-				"Хорошо,
-				я помогу тебе выбраться из нашего города",
-				"Но тебе нужно помочь мне",
-				"В нашем городе где-то лежит мечь,
-				с ним ты сможешь атаковать врагов",
-				"А с врагов падают монеты",
-				"Собери 5 монет,
-				тогда я помогу тебе",
-				"но учти, после смерти,ты потеряешь все:
-				и мечь и монеты, будь акуратен",
-				"мечь появляется каждый раз в новых местах",
-				"подсказка: атаковать ЛКМ"
+				"Хорошо,\n я помогу тебе выбраться из нашего города.",
+				"Но тебе нужно помочь мне.",
+				"В нашем городе где-то лежит меч,\n с ним ты сможешь атаковать врагов.",
+				"А с врагов падают монеты.",
+				"Собери 5 монет, тогда я помогу тебе.",
+				"Но учти: после смерти ты потеряешь всё:\nи меч, и монеты. Будь аккуратен!",
+				"Меч появляется каждый раз в новых местах.",
+				"Подсказка: атаковать на ЛКМ."
 			]
 			Global.talked = 1
 			body.start_dialogue(story)
-			
-		if (Global.coins >=5 and Global.talked == 1):
+		
+		elif Global.talked == 1 and Global.coins >= 5:
 			var story = [
 				"Уже собрал???",
-				"Хорошо, Сейчас скажу где находится первая деталь,
-				необходимая для твоего спасения",
-				"Найди справочник по нашему городу",
-				"Подсказка: это ярко красная книга",
-			]
+				"Хорошо. Сейчас скажу,\n где находится первая деталь,\n необходимая для твоего спасения.",
+				"Найди справочник по нашему городу.",
+				"Подсказка: это ярко-красная книга.",
+				"Лежит в библиотке",
+		]
 			body.start_dialogue(story)
 			body.coins = 0
 			body.update_ui()
 			Global.talked = 2
-			
-		if Global.talked == 2 and Global.has_book == false:
+
+		elif Global.talked == 2 and Global.has_book == false:
 			var story = [
-				"Напоминаю нужна книга",
-				"Красная",
-			]
+				"Напоминаю: нужна книга.",
+				"Красная!",
+		]
 			body.start_dialogue(story)
-		
-		
-		if (Global.talked == 2) and (Global.has_book == true):
+
+		elif Global.talked == 2 and Global.has_book == true:
 			var story = [
-				"Молодец у тебя теперь есть первая деталь,
-				необходимая для твоего спасения",
-				"Но то, где находится следующая, я скажу тебе
-				за 5 монет",
-				"Поэтому в путь",
-			]
+				"Молодец, у тебя теперь есть первая деталь,\n необходимая для твоего спасения.",
+				"Но то, где находится следующая, \nя скажу тебе за 5 монет.",
+				"Поэтому в путь!"
+		]
 			Global.talked = 3
 			body.start_dialogue(story)
-			
-			
-			
-		if (Global.talked == 3) and (Global.coins <5):
+		
+		elif Global.talked == 3 and Global.coins < 5:
 			var story = [
-				"5 монет надо",
+				"5 монет надо.",
+		]
+			body.start_dialogue(story)
+
+		elif Global.talked == 3 and Global.coins >= 5:
+			var story = [
+				"О, молодец! \nСледующая деталь — это цветок, найди его.",
+				"Подсказка: это куст розы."
+		]
+			body.start_dialogue(story)
+			body.coins = 0
+			body.update_ui()
+			Global.talked = 4
+		
+		elif Global.talked == 4 and Global.has_zvetok == false:
+			var story = [
+				"Я жду куст розы. Без него дело не пойдет.",
+		]
+			body.start_dialogue(story)
+		
+		elif Global.talked == 4 and Global.has_zvetok == true:
+			var story = [
+				"Какая красота... Цветок у нас.",
+				"Осталась последняя деталь — \nтайная склянка с эссенцией города.",
+				"Но\n за красивые глаза я информацию не раздаю.\n Ещё 5 монет, и мы в расчете."
+		]
+			Global.talked = 5
+			body.start_dialogue(story)
+
+		elif Global.talked == 5 and Global.coins < 5:
+			var story = [
+				"Принеси еще 5 монет, и я скажу, где искать банку.",
 			]
 			body.start_dialogue(story)
 		
-		if (Global.talked == 3) and (Global.coins <=5):
+		elif Global.talked == 5 and Global.coins >= 5:
 			var story = [
-				"о молодец, следующая деталь это цветок, найти его",
-				"подсказка: он ярко фиолетовый"
+				"Отлично! Склянка спрятана в школе.",
+				"На столе стоит две банки, \nта что меньше - твоя",
+				"Найди её и возвращайся. Это последний шаг!"
 			]
 			body.start_dialogue(story)
 			body.coins = 0
 			body.update_ui()
+			Global.talked = 6
+
+		elif Global.talked == 6 and Global.has_banka == false:
+			var story = [
+				"Осталось принести только банку.\n Поторопись!",
+			]
+			body.start_dialogue(story)
+		
+		elif Global.talked == 6 and Global.has_banka == true:
+			var story = [
+				"Невероятно... \nТы собрал всё: и книгу, и цветок, и банку!",
+				"Я держу своё слово. \nДревний ритуал завершен, и путь из города открыт.",
+				"Ты свободен! Спасибо тебе..."
+			]
+			body.start_dialogue(story)
+			win_game()
 			
 			
 func _on_body_exited(body: Node2D) -> void:
 	if body.name.to_lower() == "player":
 		body.hide_dialogue()
+		
+func win_game():
+	await get_tree().create_timer(5.0).timeout
+	Global.talked = 0
+	Global.coins = 0
+	Global.has_sword = false
+	Global.has_book = false
+	Global.has_zvetok = false
+	Global.has_banka = false
+
+	get_tree().change_scene_to_file("res://scene/win.tscn")
